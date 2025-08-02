74e8c24dab28cb528fe37f523b3038b534eb83bb
psremilio <emilio.f.passaro@gmail.com>
2025-08-02 13:59:29 -0700

    feat: load proxies at startup and use for requests

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "[proxy] lade Liste …"
+if curl -fSL "$PROXY_LIST_URL" -o proxies.txt ; then
+  echo "[proxy] $(wc -l < proxies.txt) Einträge gespeichert"
+else
+  echo "[proxy] FEHLER beim Download!" >&2
+fi
+
+node main.js
