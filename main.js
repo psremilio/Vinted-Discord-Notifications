6021406b2cc86ed7d1875de4a2844a05d76f4662
psremilio <emilio.f.passaro@gmail.com>
2025-09-05 04:27:52 +0200

    fix(pre-ack): import EdfGate and use deferReply(flags) with short EDF pause for every slash interaction

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ import { initProxyPool, startHeartbeat, stopHeartbeat, healthyCount, coolingCoun
 import { state } from './src/state.js';
 import { get as httpGet } from './src/net/http.js';
 import { metrics, serializeMetrics } from './src/infra/metrics.js';
-import { activeSearches } from './src/run.js';
+import { activeSearches } from './src/run.js';\nimport { EdfGate } from './src/schedule/edf.js';
 import { startLoopLagMonitor, getLagP95 } from './src/infra/loopLag.js';
 import { rateCtl } from './src/schedule/rateControl.js';
 
@@ -288,5 +288,6 @@ function shutdown(signal){
 }
 process.on('SIGTERM', () => shutdown('SIGTERM'));
 process.on('SIGINT', () => shutdown('SIGINT'));
+
 
 
